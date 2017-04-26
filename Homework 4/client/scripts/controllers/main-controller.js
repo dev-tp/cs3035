bfa6cb3029fa4e0235a1608d726226b985ce4301
Eryk <rcksorto@yahoo.com>
2017-04-26 03:05:43 -0700

    Implemented POST method; New reminders can be added

--- Homework 4/client/scripts/controllers/main-controller.js
+++ Homework 4/client/scripts/controllers/main-controller.js
@@ -1,5 +1,35 @@
 angular
   .module('mainController', [])
-  .controller('MainController', function($scope) {
+  .controller('MainController', function($http, $scope) {
+    $scope.addReminder = function() {
+      if ($scope.title && $scope.due) {
+        var todo = {
+          title: $scope.title,
+          description: $scope.description,
+          due: $scope.due,
+          done: false
+        };
 
+        $http.post('/todolist', todo).then(function(response) {
+          $scope.title = '';
+          $scope.description = '';
+          $scope.due = '';
+
+          refresh();
+        }, function(error) {
+          console.log('[ ERROR ]' + JSON.stringify(error));
+          refresh();
+        });
+      }
+    };
+
+    function refresh() {
+      $http.get('/todolist').then(function(response) {
+        $scope.todos = response.data;
+      }, function(error) {
+        console.log('There was an error refreshing the page!');
+      });
+    }
+
+    refresh();
   });
--- Homework 4/server.coffee
+++ Homework 4/server.coffee
@@ -1,11 +1,25 @@
+bodyparser = require 'body-parser'
 express = require 'express'
+mongojs = require 'mongojs'
 
 app = do express
+database = mongojs 'todolist', ['todolist']
 
+app.use bodyparser.json()
 app.use express.static('client')
 
 app.get '/', (request, response) ->
   response.sendFile 'client/index.html'
+
+app.get '/todolist', (request, response) ->
+  database.todolist.find (error, objects) ->
+    response.json [] if error
+    response.json objects
+
+app.post '/todolist', (request, response) ->
+  database.todolist.save request.body, (error, objects) ->
+    response.json [] if error
+    response.json objects
 
 app.listen 8080, ->
   console.log 'Server is running on localhost, port 8080'
